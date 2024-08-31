d259231f6e4f34f249c2873d5e1e6b0f3aad73ef
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-08-30 20:51:02 -0400

    Add function getHumanChoice
    This function prompts the user for a string input and returns the input.
    The input must be either "rock", "paper", or "scissors"
    (not case sensitive) otherwise it will return "Invalid Input".

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function getComputerChoice() {
             return "huh?";
     }
 }
+
+function getHumanChoice() {
+    let choice = prompt("Rock, paper, scissors. Shoot!");
+    // Make sure choice is not falsy first, performing .toLowerCase in declaration throws
+    // TypeError if choice is null (I.E. clicking cancel).
+    if (choice) {
+        choice.toLowerCase();
+    }
+    if (choice == "rock" || choice == "paper" || choice == "scissors") {
+        return choice;
+    }
+    else {
+        return "Invalid Input";
+    }
+}
