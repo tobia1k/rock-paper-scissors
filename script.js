7de8c32ceea77eb0d954f6b05e01d6b185600fee
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-08-30 20:28:43 -0400

    Add function getComputerChoice
    This function returns either "rock", "paper", or "scissors"
    depending on the choice variable which will contain a random
    number from 0 to 2. The switch function determines which string
    to return based on the value of the choice variable.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    switch(choice) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2: 
+            return "scissors";
+            break;
+        default:
+            return "huh?";
+    }
+}
